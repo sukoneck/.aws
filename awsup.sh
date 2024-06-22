5b9f56641072cbdfc36b5a231ab2cdabf3008d3c
Daniel Sukoneck <daniel@sukoneck.com>
2024-06-21 21:38:47 -0700

    fix path

--- awsup.sh
+++ awsup.sh
@@ -34,8 +34,8 @@ handle_file_backup() {
 
     # Only backup the files, not subdirectories
     for item in "${TARGET_DIR}"/*; do
-        if [ -f "$item" ]; then
-            mv "$item" "${BACKUP_DIR}"
+        if [ -f "${item}" ]; then
+            mv "${TARGET_DIR}/${item}" "${BACKUP_DIR}"
         fi
     done
 }
